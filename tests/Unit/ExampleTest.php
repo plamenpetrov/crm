2ffbbb15a1a3a3d086e4dbb6b56be11f6f431aab
PLAMEN PETROV <plamen.milchev.petrov@gmail.com>
2020-09-02 16:41:33 +0300

    Remove tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -12,9 +12,10 @@ class ExampleTest extends TestCase
      * A basic test example.
      *
      * @return void
-     */
+   
     public function testBasicTest()
     {
         $this->assertTrue(true);
     }
+    */
 }
